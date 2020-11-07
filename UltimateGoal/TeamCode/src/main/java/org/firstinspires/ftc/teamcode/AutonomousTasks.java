12f5c1676574e267ad12ffdd011e8cda094c0a75
Rohan <40905346+Rohan1215@users.noreply.github.com>
2020-11-07 09:58:03 -0800

    Update AutonomousTasks.java

--- UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTasks.java
+++ UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTasks.java
@@ -24,9 +24,12 @@ public class AutonomousTasks{
     int numberOfRings;
     Odometry odometry;
     ChassisSerialMotion chassisSerialMotion;
+<<<<<<< HEAD
     public static final double DISTANCE_TO_TARGET_ZONE_A = 127.64; // calculated using field specifications (80.25 - (11.5 + 18)) * 2.54
     public static final double DISTANCE_TO_TARGET_ZONE_B = 186.06; // <-- need to be converted to coordinates
     public static final double DISTANCE_TO_TARGET_ZONE_C = 244.48;
+=======
+>>>>>>> parent of 9367593... updated autonomous tasks with whacker and launcher
 
     public AutonomousTasks(OpMode opmode){
         op = opmode;
@@ -56,7 +59,11 @@ public class AutonomousTasks{
 
     }
 
+<<<<<<< HEAD
     public void launchRings() {
+=======
+    public void launchRingsAtPowerShots() {
+>>>>>>> parent of 9367593... updated autonomous tasks with whacker and launcher
 
     }
 
@@ -77,7 +84,44 @@ public class AutonomousTasks{
 
             case INIT:
                 init();
+<<<<<<< HEAD
                 state = State.GRAB_WOBBLE;
+=======
+                state = State.MOVE_TO_POWER_SHOT_LAUNCH_POSITION;
+                break;
+
+            case MOVE_TO_POWER_SHOT_LAUNCH_POSITION:
+                if(chassisSerialMotion.getState() == ChassisStateMachine.State.STOP){
+                    state = State.LAUNCH_RINGS_AT_POWER_SHOTS;
+                    chassisSerialMotion.run(); //so that the STOP state in ChassisStateMachine can run
+                    break;
+                }
+                if(chassisSerialMotion.getState()!=ChassisStateMachine.State.EXECUTE){
+                    moveToPowerShotLaunchPosition(); //this cannot be called multiple times
+                }
+                chassisSerialMotion.run();
+                break;
+
+            case LAUNCH_RINGS_AT_POWER_SHOTS:
+                launchRingsAtPowerShots();
+                state = State.MOVE_TO_TARGET_ZONE;
+                break;
+
+            case MOVE_TO_TARGET_ZONE:
+                if(chassisSerialMotion.getState() == ChassisStateMachine.State.STOP){
+                    if(numberOfRings == 0 || numberOfRings == 1){
+                        state = State.GRAB_WOBBLE;
+                    }
+                    else if(numberOfRings == 4){
+                        state = State.PICK_UP_RINGS;
+                    }
+                    break;
+                }
+                if(chassisSerialMotion.getState() != ChassisStateMachine.State.EXECUTE){
+                    moveToTargetZone();
+                }
+                chassisSerialMotion.run();
+>>>>>>> parent of 9367593... updated autonomous tasks with whacker and launcher
                 break;
 
             case GRAB_WOBBLE:
