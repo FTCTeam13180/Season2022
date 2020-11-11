c5ae60f4270a2b04781ed8f5e6b124bff3b4eb59
Yash Pradhan <yashpradhan181@gmail.com>
2020-11-11 14:47:14 -0800

    updates ChassisComponent
    Added the new final variables for Gobilda motors
    Added initDriveHardwareMap
    Added AnyMecanum, now renamed to mecanumDrive

--- UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisComponent.java
+++ UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisComponent.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by Yash Pradhan on 10/4/20
@@ -19,28 +20,44 @@ public class ChassisComponent {
     private DcMotor topr;
     private DcMotor rearr;
     private DcMotor rearl;
-    private static final double COUNTS_PER_MOTOR_REV = 1120 ;    // eg: Andymark Motor Encoder
-    private static final double DRIVE_GEAR_REDUCTION = 0.776 ;     // This is < 1.0 if geared up
-    private static final double WHEEL_DIAMETER_CM = 10.16 ;
-    private static final double COUNTS_PER_CM  = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_CM * 3.1415);
-    private static final double STRAIGHT_SLIPPAGE_CORRECTION = 96/97.5;
+    //no gear reduction ratio as of now
+    double wheel_diameter = 9.60798; //cm
+    double cntsPerRotation = 1440;
+    double cntsPerCm = (1/(Math.PI*wheel_diameter))*cntsPerRotation;
+  
+   //private static final double COUNTS_PER_MOTOR_REV = 1120 ;    // eg: Andymark Motor Encoder
+    //private static final double DRIVE_GEAR_REDUCTION = 0.776 ;     // This is < 1.0 if geared up
+    //private static final double WHEEL_DIAMETER_CM = 10.16 ;
+    //private static final double COUNTS_PER_CM  = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_CM * 3.1415);
+    //private static final double STRAIGHT_SLIPPAGE_CORRECTION = 96/97.5;
 
-    public void init() {
-        topl = opMode.hardwareMap.get(DcMotor.class, "Topl");
-        topr = opMode.hardwareMap.get(DcMotor.class, "Topr");
-        rearl = opMode.hardwareMap.get(DcMotor.class, "Rearl");
-        rearr = opMode.hardwareMap.get(DcMotor.class, "Rearr");
-        topl.setDirection(DcMotor.Direction.FORWARD);
-        topr.setDirection(DcMotor.Direction.FORWARD);
-        rearl.setDirection(DcMotor.Direction.FORWARD);
-        rearr.setDirection(DcMotor.Direction.FORWARD);
-        topl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    public void initDriveHardwareMap(){
+        topr = opMode.hardwareMap.dcMotor.get("TopR");
+        topl = opMode.hardwareMap.dcMotor.get("TopL");
+        rearr = opMode.hardwareMap.dcMotor.get("BackR");
+        rearl = opMode.hardwareMap.dcMotor.get("BackL");
+
+        topr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        topl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rearl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        topr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        topl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rearr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rearl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         topr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rearl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        topl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        opMode.telemetry.addData("Chassis:", "Initialized");
+        rearl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rearr.setDirection(DcMotorSimple.Direction.REVERSE);
+        topr.setDirection(DcMotorSimple.Direction.REVERSE);
+        topl.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        opMode.telemetry.addData("Status", "Hardware Map Init Complete");
+        opMode.telemetry.update();
     }
 
     public void move(double toprPower, double toplPower, double rearrPower, double rearlPower) {
@@ -58,10 +75,17 @@ public class ChassisComponent {
     }
 
     public void setTargetPosition(double cms) {
-        topl.setTargetPosition((int) (topl.getCurrentPosition() + (cms * COUNTS_PER_CM * STRAIGHT_SLIPPAGE_CORRECTION)));
-        topr.setTargetPosition((int) (topr.getCurrentPosition() + (cms * COUNTS_PER_CM * STRAIGHT_SLIPPAGE_CORRECTION)));
-        rearl.setTargetPosition((int) (rearl.getCurrentPosition() + (cms * COUNTS_PER_CM * STRAIGHT_SLIPPAGE_CORRECTION)));
-        rearr.setTargetPosition((int) (rearr.getCurrentPosition() + (cms * COUNTS_PER_CM * STRAIGHT_SLIPPAGE_CORRECTION)));
+        topl.setTargetPosition((int) (topl.getCurrentPosition() + (cms * cntsPerCm)));
+        topr.setTargetPosition((int) (topr.getCurrentPosition() + (cms * cntsPerCm)));
+        rearl.setTargetPosition((int) (rearl.getCurrentPosition() + (cms * cntsPerCm)));
+        rearr.setTargetPosition((int) (rearr.getCurrentPosition() + (cms * cntsPerCm)));
+    }
+    public void mecanumDrive(double x,double y, double power_scale){
+        double power = Math.sqrt(x * x + y * y);
+        topr.setPower(power_scale*(y-x)/power);
+        topl.setPower(power_scale*(x+y)/power);
+        rearr.setPower(power_scale*(x+y)/power);
+        rearl.setPower(power_scale*(y-x)/power);
     }
 
     public void logCurrentPosition () {
