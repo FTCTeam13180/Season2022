1bec080728973a29b493a1f6971f5e1667e601f6
Pratyush Seth <pratyushs@fb.com>
2020-11-17 23:33:03 -0800

    Change Requests to cleanup code in Chassis Component

--- UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisComponent.java
+++ UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisComponent.java
@@ -24,12 +24,15 @@ public class ChassisComponent {
     double wheel_diameter = 9.60798; //cm
     double cntsPerRotation = 1440;
     double cntsPerCm = (1/(Math.PI*wheel_diameter))*cntsPerRotation;
-  
+
+    /// CR-1 START ///
+    /// Remove all the following constants ///
    //private static final double COUNTS_PER_MOTOR_REV = 1120 ;    // eg: Andymark Motor Encoder
     //private static final double DRIVE_GEAR_REDUCTION = 0.776 ;     // This is < 1.0 if geared up
     //private static final double WHEEL_DIAMETER_CM = 10.16 ;
     //private static final double COUNTS_PER_CM  = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_CM * 3.1415);
     //private static final double STRAIGHT_SLIPPAGE_CORRECTION = 96/97.5;
+    /// CR-1 END ///
 
     public void initDriveHardwareMap(){
         topr = opMode.hardwareMap.dcMotor.get("TopR");
@@ -37,6 +40,8 @@ public class ChassisComponent {
         rearr = opMode.hardwareMap.dcMotor.get("BackR");
         rearl = opMode.hardwareMap.dcMotor.get("BackL");
 
+        /// CR-2 Start ///
+        /// Remove all the RunMOde set calls. These are needed for autonomous mode so we should set them in Odometry code only. ///
         topr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         topl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rearr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -46,6 +51,7 @@ public class ChassisComponent {
         topl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rearr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rearl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        /// CR-2 End ///
 
         topr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         topl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -54,18 +60,24 @@ public class ChassisComponent {
 
         rearr.setDirection(DcMotorSimple.Direction.REVERSE);
         topr.setDirection(DcMotorSimple.Direction.REVERSE);
+        /// CR-3 Start ///
+        /// We need to reverse direction only for right motors and not for left motor, remove the following line ///
         topl.setDirection(DcMotorSimple.Direction.REVERSE);
+        /// CR-3 End ///
 
         opMode.telemetry.addData("Status", "Hardware Map Init Complete");
         opMode.telemetry.update();
     }
 
+    /// CR-4 Start ///
+    /// Remove this method, we won't need it ///
     public void move(double toprPower, double toplPower, double rearrPower, double rearlPower) {
         topr.setPower(toplPower);
         topl.setPower(toprPower);
         rearr.setPower(rearrPower);
         rearl.setPower(rearlPower);
     }
+    /// CR-4 End ///
 
     public void setRunMode(DcMotor.RunMode runMode) {
         topl.setMode(runMode);
@@ -80,6 +92,7 @@ public class ChassisComponent {
         rearl.setTargetPosition((int) (rearl.getCurrentPosition() + (cms * cntsPerCm)));
         rearr.setTargetPosition((int) (rearr.getCurrentPosition() + (cms * cntsPerCm)));
     }
+
     public void mecanumDrive(double x,double y, double power_scale){
         double power = Math.sqrt(x * x + y * y);
         topr.setPower(power_scale*(y-x)/power);
@@ -93,6 +106,7 @@ public class ChassisComponent {
     }
 
     public void stop() {
+        /// CR-5: Instead of calling move, which we are removing, setPower explicitly for all 4 motors to 0 here. ///
         move(0, 0, 0, 0);
     }
 
