674bca18201b6b64898a1c6a9aeb6560d70f475e
Rithwik Seth <rithwikseth@gmail.com>
2020-12-02 22:29:23 -0800

    fix it felix

--- UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StackerComponent.java
+++ UltimateGoal/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StackerComponent.java
@@ -4,9 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class StackerComponent {
-    private static final double STACKER_DOWN = 0;
-    private static final double STACKER_DUMP = 1;
-    private static final double STACKER_UP = 0.35;
+    private static final double STACKER_DOWN = 1;
+    private static final double STACKER_DUMP = 0;
+    private static final double STACKER_UP = 0.635;
     private static final double WHACKER_OUT = 0;
     private static final double WHACKER_IN = 1;
     private Servo stacker;
